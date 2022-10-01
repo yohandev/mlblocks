bf71b30a9d1faae78353ecf91147f6698b2b3612
yohandev <yohang@mit.edu>
2022-10-01 15:32:05 -0400

    Project setup

---
+++ src/index.js
@@ -0,0 +1,13 @@
+// Code conventions:
+// Use const wherever possible, let otherwise(never use var, fuck var).
+// No newline before brackets/parentheses(ie. functions or below).
+// Use lambdas over functions, or methods in classes
+// No semicolons if you don't need them(me and the boys hate them)
+// Commands:
+// Build using `yarn run build`(make sure you `yarn init` first to
+// download dependencies the first time)
+import $ from 'jquery'
+
+const abc = $(`<h1>Hello, World</h1>`)
+
+abc.appendTo(document.body)
